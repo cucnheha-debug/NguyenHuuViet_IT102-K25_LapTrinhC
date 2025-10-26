f0e2d4a5bbc3d16bd09cc8d2b673d31192db39f1
cucnheha-debug <cucnheha@gmail.com>
2025-10-26 22:26:30 +0700

    Update bt1.cpp

--- ses 3/bt1.cpp
+++ ses 3/bt1.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int main(){
-	
-	char sentence [50] = "skibidi";
-	printf("ban ten la %s\n", sentence);
-	
-	return 0;
-	
+
+int main() {
+    char name[50]; // khai báo biến để lưu tên (tối đa 49 ký tự)
+
+    printf("Nhap ten cua ban: ");
+    fgets(name, sizeof(name), stdin); // đọc chuỗi từ bàn phím (cho phép có khoảng trắng)
+
+    // In lời chào
+    printf("Xin chao %s", name);
+
+    return 0;
 }
